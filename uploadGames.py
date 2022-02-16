46dcfb6e786fbf0d825073f06b861425dc9df423
Legend <85244055+LegendGamer885@users.noreply.github.com>
2022-02-16 21:24:32 +0530

    Update uploadGames.py

--- uploadGames.py
+++ uploadGames.py
@@ -7,31 +7,20 @@ class TransferData:
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
 
-        f = open(file_from,"rb")
-        dbx.files_upload(f.read(),file_to)
 
-def uploadFile(image_name):
-    access_token = 'sqAnqpyuEbEAAAAAAAAAAeq5g1lN-CiRqZXtpVgrcWvVqkN6WuEN3WN-TVd_WFcV'
-    
-    file = image_name
-    file_from = file
-    file_to = "/test images/" + (image_name)
-
-    dbx = dropbox.Dropbox(access_token)
-    with open(file_from,"rb") as f:
-        dbx.files_upload(f.read(),file_to,mode = dropbox.files.WriteMode.overwrite)
-        print("file uploaded")
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
 
 def main():
-    access_token = 'sqAnqpyuEbEAAAAAAAAAAeq5g1lN-CiRqZXtpVgrcWvVqkN6WuEN3WN-TVd_WFcV'
+    access_token = 'Gt4_HKmnMrwAAAAAAAAAAVljqWvm-Wbp-15SeNnBu98wD3QjvlUOZucUHN6t8HuY'
     transferData = TransferData(access_token)
 
-    file_from = input("Enter the file path to transfer- ")
-    file_to = input("Enter the full path to upload to dropbox- ")
+    file_from = input("Enter the Game file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
 
     # API v2
     transferData.upload_file(file_from, file_to)
-    print("File has been uploaded")
+    print("file has been moved !!!")
 
-if __name__ == '__main__':
-    main()
+
+main()
